e4573eb2e35dcfdc5779ba3a3d4447a93bab93b7
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-05-31 15:03:27 +0200

    coefficients: fix dice coefficient definition

--- coefficients.py
+++ coefficients.py
@@ -16,4 +16,4 @@ def jaccard(a:set, b:set) -> float:
     return len(a & b) / (len(a | b))
 
 def dice(a:set, b:set) -> float:
-    return 2 * len(a | b) / (len(a) + len(b))
+    return 2 * len(a & b) / (len(a) + len(b))
