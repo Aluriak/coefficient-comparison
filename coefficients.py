e08695f25244e56a1397cfaf821810448019c64d
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2016-05-31 14:28:10 +0200

    coefficients: functions() is not a generator, returns all func as tuple

--- coefficients.py
+++ coefficients.py
@@ -6,10 +6,10 @@ with return value in [0;1].
 """
 
 
-def functions() -> iter:
-    """Yield all functions of this module, excluding this one"""
-    yield from (attr for name, attr in globals().items()
-                if callable(attr) and name != 'functions')
+def functions() -> tuple:
+    """Return functions of this module, excluding this one"""
+    return tuple(attr for name, attr in globals().items()
+                 if callable(attr) and name != 'functions')
 
 
 def jaccard(a:set, b:set) -> float:
